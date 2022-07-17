880b79986aef68370b3ed3cfd0074ad21a0f7a1f
Anton <ag629331@gmail.com>
2022-07-17 16:27:57 +0300

    minor update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "MatrixMemory.h"
 #include <QtWidgets/QApplication>
-
+#include "gameField.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MatrixMemory w;
+    GameField gf;
+    gf.show();
     w.show();
     return a.exec();
 }
